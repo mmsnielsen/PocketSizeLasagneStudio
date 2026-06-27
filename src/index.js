c65cf73b3bb847b112ddf8e8c34e13f1f6479bb8
etbordtilen-lang <etbordtilen@gmail.com>
2026-06-27 16:36:59 +0200

    Added JS to header
    Added a scroll function so when you start to scroll and line break appears to hight light the header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 // Header section //
+
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 20) {
+    header.classList.add("header--scrolled");
+  } else {
+    header.classList.remove("header--scrolled");
+  }
+});
