60961b7935cdab034cda701bd914d4a57c54b22c
etbordtilen-lang <etbordtilen@gmail.com>
2026-07-22 17:24:20 +0200

    Updated projects section
    Edited HTML so it structure aligns more with layout in Figma design. Added CSS. Added JS dropdown function for more details on projects

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// Header section //
+// HEADER SECTION //
 
 const header = document.querySelector(".header");
 
@@ -8,4 +8,33 @@ window.addEventListener("scroll", () => {
   } else {
     header.classList.remove("header--scrolled");
   }
+});
+
+// PROJECTS SECTION //
+
+const projectButtons = document.querySelectorAll(".projects-trigger-btn");
+
+projectButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const contentId = button.getAttribute("aria-controls");
+    const contentDrawer = document.getElementById(contentId);
+
+    const isExpanded = button.getAttribute("aria-expanded") === "true";
+
+    button.setAttribute("aria-expanded", !isExpanded);
+
+    if (contentDrawer) {
+      contentDrawer.setAttribute("aria-hidden", isExpanded);
+    }
+
+    // Rotates the '+' icon to 'x' symbol when open //
+    const icon = button.querySelector(".projects-icon");
+    if (icon) {
+      if (!isExpanded) {
+        icon.style.transform = "rotate(45deg)";
+      } else {
+        icon.style.transform = "rotate(0deg)";
+      }
+    }
+  });
 });
